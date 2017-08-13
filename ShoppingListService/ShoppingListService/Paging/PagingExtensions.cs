1c36c8106b672c58f5dca45d22e9274ee2538e25
slyrat <sly.rat.peon@gmail.com>
2017-08-13 13:48:56 +0100

    Get paging service implemented

---
+++ ShoppingListService/ShoppingListService/Paging/PagingExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingListService.Paging
+{
+    public static class PagingExtensions
+    {
+        public static List<T> PageAndSort<T>(
+            this List<T> data, 
+            int? pageSize = null, 
+            int? page = null, 
+            string sortByProperty = null, 
+            bool sortAscending = true)
+            where T:  new()
+        {
+            if (sortByProperty != null)
+            {
+                var propertyInfo = typeof(T).GetProperty(sortByProperty);
+                // do sorting
+                if (propertyInfo != null)
+                {
+                    if (sortAscending)
+                    {
+                        data = data.OrderBy(d => propertyInfo.GetValue(d)).ToList();
+                    }
+                    else
+                    {
+                        data = data.OrderByDescending(d => propertyInfo.GetValue(d)).ToList();
+                    }
+                }
+            }
+
+            if (pageSize.HasValue && page.HasValue &&
+                page > 0 && pageSize > 0)
+            {
+                // do paging
+                var totalNumber = data.Count;
+                // only need to do something if the page is smaller than total
+                if (pageSize < totalNumber)
+                {
+                    // if >= total it means we are trying to start
+                    // after the last item in the list
+                    if (((page - 1) * pageSize) < totalNumber)
+                    {
+                        data = data.Skip((page.Value - 1) * pageSize.Value).Take(pageSize.Value).ToList();
+                    }
+                    else
+                    {
+                        // trying to get beyond end, give them nothing back
+                        data = new List<T>();
+                    }
+                }
+                else
+                {
+                    // trying to get beyond end, give them nothing back
+                    data = new List<T>();
+                }
+            }
+
+            return data;
+        }
+    }
+}
---
+++ ShoppingListService/ShoppingListTests/PagingExtensionsTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using NUnit.Framework;
+using ShoppingListService.Storage;
+using ShoppingListService.Contracts;
+using System.Collections.Generic;
+using Moq;
+using ShoppingListService.Paging;
+
+namespace ShoppingListTests
+{
+    [TestFixture]
+    public class PagingExtensionsTests
+    {
+        /// <summary>
+        /// Gets a list of 5 drinks
+        /// </summary>
+        /// <returns></returns>
+        public List<Drink> GetDrinkData()
+        {
+            return new List<Drink>
+            {
+                new Drink
+                {
+                    Name = "Coke",
+                    Number = 4
+                },
+                new Drink
+                {
+                    Name = "Pepsi",
+                    Number = 5
+                },
+                new Drink
+                {
+                    Name = "Aspalls",
+                    Number = 6
+                },
+                new Drink
+                {
+                    Name = "Strongbow",
+                    Number = 2
+                },
+                new Drink
+                {
+                    Name = "Guinness",
+                    Number = 1
+                }
+            };
+        }
+
+        [Test]
+        public void PageAndSort_ReturnsAllItemsWithNoPagingParams()
+        {
+            var data = GetDrinkData();
+            var initialCount = data.Count;
+
+            // Act
+            data.PageAndSort();
+
+            // Assert
+            Assert.AreEqual(initialCount, data.Count);
+        }
+
+        [Test]
+        public void PageAndSort_SortsByThePropertyGiven()
+        {
+            var data = GetDrinkData();
+            var firstItem = data[0];
+
+            // Act
+            data = data.PageAndSort(sortByProperty: nameof(firstItem.Name));
+
+            // Assert
+            Assert.AreEqual("Aspalls", data[0].Name);
+        }
+
+        [TestCase(1, 1, 1, "Coke")]
+        [TestCase(2, 1, 2, "Coke")]
+        [TestCase(2, 2, 2, "Aspalls")]
+        [TestCase(2, 3, 1, "Guinness")]
+        [TestCase(3, 3, 0, "")] // Assumes that if you try to get beyond end, just give them empty list
+        public void PageAndSort_PagesCorrectly(int pageSize, int page, int ExpectedCount, string ExpectedNameOfFirstItem)
+        {
+            var data = GetDrinkData();
+            
+            // Act
+            data = data.PageAndSort(page: page, pageSize: pageSize);
+
+            // Assert
+            Assert.AreEqual(ExpectedCount, data.Count, "count did not match");
+            if (ExpectedCount > 0)
+            {
+                Assert.AreEqual(ExpectedNameOfFirstItem, data[0].Name);
+            }
+        }
+    }
+}
